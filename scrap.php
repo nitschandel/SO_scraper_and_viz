2a74f8e667f1a5cf0a76d146d25cb74761bd713f
Nitesh Chandel <nitesh.chandel61@gmail.com>
2014-10-19 21:33:56 +0530

    Create scrap.php

---
+++ scrap.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//cdn.jsdelivr.net/jquery/1.9.1/jquery-1.9.1.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col-md-4"></div>
+<div class="col-md-8">
+<h1><b> Visualisation</b> </h1>
+</div>
+</div>
+<br/><br/>
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-2"></div>
+  <div class="col-md-10">
+  <button onclick="location.href = 'tree.html';" id="myButton" class="btn btn-primary btn-lg" >Chart for Questions</button>
+  </div>
+  </div>
+  <br/>
+  <div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-8">
+  <button onclick="location.href = 'piechart.html';" id="myButton" class="btn btn-primary btn-lg" >Chart for Featured Questions</button>
+  </div>
+  </div>
+  <br/>
+  <div class="row">
+  <div class="col-md-6"></div>
+  <div class="col-md-6">
+  <button onclick="location.href = 'barchart.html';" id="myButton" class="btn btn-primary btn-lg" >Chart for Unanswered Questions</button>
+  </div>
+  </div>
+  </div>
+  </div>
+</body>
+</html>
